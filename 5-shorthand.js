a113d5bba9d91a1f64046665e3f082aa7e2ce1d8
mnjit20 <mnjit.vrma@gmail.com>
2018-07-01 00:41:48 +0530

    shorthand done

--- 5-shorthand.js
+++ 5-shorthand.js
@@ -16,3 +16,33 @@ let team = {
 console.log(person);
 
 console.log(team);
+
+
+console.log("--------------------------");
+
+
+var color = "red";
+var speed = 10;
+var drive = 'fun1';
+var drive2 = 'fun2';
+
+//defining objects with shorthand function declaration
+var object = {
+  color,
+  speed,
+  [drive]: function () {
+    console.log('vroom');
+  },
+  [drive2]: function () {
+    console.log('vroom2');
+  },
+  go() {
+    console.log('go function');
+  }
+};
+
+console.log(object.color);
+console.log(object.speed);
+object.fun1();
+object.fun2();
+object.go();
