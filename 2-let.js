6d38905cb14f56062fbc071b86dba4971209eaf8
mnjit20 <mnjit.vrma@gmail.com>
2018-06-30 23:48:55 +0530

    let done

--- 2-let.js
+++ 2-let.js
@@ -11,3 +11,40 @@ for (let i = 0; i < 10; i++) {
 fs.forEach(function (aa) {
   aa();
 });
+
+console.log('---------------');
+console.log('---------------');
+// different function defined.
+
+function varFunction() {
+  var previous = 0;
+  var current = 1;
+  var i;
+  var temp;
+  for (i = 0; i < 10; i += 1) {
+    temp = previous;
+    previous = current;
+    current = temp + current;
+    console.log('temp :', temp, 'previous: ', previous, ' current : ', current);
+    console.log();
+  }
+}
+
+
+function letFunction() {
+  let previous = 0;
+  let current = 1;
+  let i;
+  //  let temp;
+  for (i = 0; i < 10; i += 1) {
+    let temp = previous;
+    previous = current;
+    current = temp + current;
+    console.log('temp :', temp, 'previous: ', previous, ' current : ', current);
+    console.log();
+  }
+}
+
+varFunction();
+console.log('---------------');
+letFunction();
